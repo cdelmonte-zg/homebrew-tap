1edf32de62178523bd61121f173211a08b31d2f5
Christian Del Monte <christian.del-monte@posteo.de>
2026-06-11 18:14:41 +0200

    clings 0.1.1

--- Formula/clings.rb
+++ Formula/clings.rb
@@ -1,28 +1,28 @@
 class Clings < Formula
   desc "Small exercises to learn advanced C concepts - inspired by rustlings"
   homepage "https://github.com/cdelmonte-zg/clings"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/cdelmonte-zg/clings/releases/download/v0.1.0/clings-v0.1.0-aarch64-apple-darwin.tar.gz"
-      sha256 "2b49a8d74790ca20346a6a565f710cce805f59822980b4481cb38e0f410508d6"
+      url "https://github.com/cdelmonte-zg/clings/releases/download/v0.1.1/clings-v0.1.1-aarch64-apple-darwin.tar.gz"
+      sha256 "4ee60e86fb3f882dfd3cf9151d42f0cbe52a6cdd47272e730a902d74f0739573"
     end
     on_intel do
-      url "https://github.com/cdelmonte-zg/clings/releases/download/v0.1.0/clings-v0.1.0-x86_64-apple-darwin.tar.gz"
-      sha256 "c5d2efd78cef0e149e52c7df31d4402998b8c227d92bfaf1c34c02881cc02431"
+      url "https://github.com/cdelmonte-zg/clings/releases/download/v0.1.1/clings-v0.1.1-x86_64-apple-darwin.tar.gz"
+      sha256 "3faf450aa34123a71410f4f718af3a9357008bb4faab5447cd1571e8668b3639"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/cdelmonte-zg/clings/releases/download/v0.1.0/clings-v0.1.0-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "3b4573568d0d3f6a31cb8f49476e2e13b98f1205936ddd13f8f271c7fcfba155"
+      url "https://github.com/cdelmonte-zg/clings/releases/download/v0.1.1/clings-v0.1.1-aarch64-unknown-linux-musl.tar.gz"
+      sha256 "ccc876ad706cca1a25aa85574b2e1e5979d009567394d2ff04d9f0670024e674"
     end
     on_intel do
-      url "https://github.com/cdelmonte-zg/clings/releases/download/v0.1.0/clings-v0.1.0-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "890f5cd2c57084b98c01cee3263f9dc0e42c479eb61b9b60ad2eb5d1a2688af0"
+      url "https://github.com/cdelmonte-zg/clings/releases/download/v0.1.1/clings-v0.1.1-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "895fab88c1095a21f8f86372e6c9d50bf0bac9ffe706a847bab6ec48f0cf78ff"
     end
   end
 
